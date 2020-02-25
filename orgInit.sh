999043a5b892427c23485eb743218149262e3e79
mshanemc <shane.mclaughlin@salesforce.com>
2020-02-25 12:36:45 -0600

    uses new plugin stuff

---
+++ orgInit.sh
@@ -0,0 +1,6 @@
+sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx force:source:push
+sfdx force:package:install --package 04t0b000001oXW0 --wait 20 -r
+sfdx shane:heroku:repo:deploy -g mshanemc -r heroku-empty -n `basename "${PWD/mshanemc-/}"` -t autodeployed-demos
+sfdx shane:ai:playground:setupHeroku -a `basename "${PWD/mshanemc-/}"` -k
+sfdx force:org:open
