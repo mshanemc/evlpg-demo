b801f5e6e094b6b90cd03c22bb260202471f1b2b
mshanemc <shane.mclaughlin@salesforce.com>
2020-02-25 12:45:50 -0600

    opens to playground; app-destroy

--- orgInit.sh
+++ orgInit.sh
@@ -3,4 +3,4 @@ sfdx force:source:push
 sfdx force:package:install --package 04t0b000001oXW0 --wait 20 -r
 sfdx shane:heroku:repo:deploy -g mshanemc -r heroku-empty -n `basename "${PWD/mshanemc-/}"` -t autodeployed-demos
 sfdx shane:ai:playground:setupHeroku -a `basename "${PWD/mshanemc-/}"` -k
-sfdx force:org:open
+sfdx force:org:open -p /lightning/n/einsteinplay__Playground
