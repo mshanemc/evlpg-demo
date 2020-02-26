f9870c4a0b347cd45543bb4d9622ff677881e396
mshanemc <shane.mclaughlin@salesforce.com>
2020-02-26 07:49:08 -0600

    with model training

--- orgInit.sh
+++ orgInit.sh
@@ -3,5 +3,5 @@ sfdx force:source:push
 sfdx force:package:install --package 04t0b000001oXW0 --wait 20 -r
 sfdx shane:heroku:repo:deploy -g mshanemc -r heroku-empty -n `basename "${PWD/mshanemc-/}"` -t autodeployed-demos
 sfdx shane:ai:playground:setupHeroku -a `basename "${PWD/mshanemc-/}"` -k
-sfdx shane:ai:dataset:upload -f assets/imageClassification.zip
+sfdx shane:ai:dataset:upload -f assets/imageClassification.zip -n Example --train
 sfdx force:org:open -p /lightning/n/einsteinplay__Playground
