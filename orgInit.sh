826830bd7a78cca34e25f34b32444e266428e4cd
mshanemc <shane.mclaughlin@salesforce.com>
2020-03-11 16:31:25 -0500

    update playground packageVersionId

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,6 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
-sfdx force:package:install --package 04t0b000001oXW0 --wait 20 -r
+sfdx force:package:install --package 04t0b000001oXug --wait 20 -r
 sfdx shane:heroku:repo:deploy -g mshanemc -r heroku-empty -n `basename "${PWD/mshanemc-/}"` -t autodeployed-demos
 sfdx shane:ai:playground:setupHeroku -a `basename "${PWD/mshanemc-/}"` -k
 sfdx shane:ai:dataset:upload -f assets/imageClassification.zip -n Example --train
